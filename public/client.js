a660110059b5bbef222ca6fb9575bc4d807be84c
Glitch (url-shortner-aziz) <none>
2018-04-22 17:01:42 +0000

    🙎🍪 Checkpoint ./public/style.css:903892/169 ./views/index.html:903892/172 ./public/client.js:903892/1646

--- public/client.js
+++ public/client.js
@@ -3,43 +3,18 @@
 
 (function(){
   
+  // Declare variables.
+  var url_input   = $("#url-input"),
+      shorten_btn = $("shorten"),
+      url = "";
   
-  // our default array of dreams
-  const dreams = [
-    'Find and count some sheep',
-    'Climb a really tall mountain',
-    'Wash the dishes'
-  ];
+  // our default array of urls.
+  const urls = {}
   
-  // define variables that reference elements on our page
-  const dreamsList = document.getElementById('dreams');
-  const dreamsForm = document.forms[0];
-  const dreamInput = dreamsForm.elements['dream'];
+  // Get url from input when shorten is clicked.
+  shorten_btn.on("click", function() {
   
-  // a helper function that creates a list item for a given dream
-  const appendNewDream = function(dream) {
-    const newListItem = document.createElement('li');
-    newListItem.innerHTML = dream;
-    dreamsList.appendChild(newListItem);
-  }
+    if(url_input.text() !== "" || url_input.text().substr(0, ))
   
-  // iterate through every dream and add it to our page
-  dreams.forEach( function(dream) {
-    appendNewDream(dream);
-  });
-  
-  // listen for the form to be submitted and add a new dream when it is
-  dreamsForm.onsubmit = function(event) {
-    // stop our form submission from refreshing the page
-    event.preventDefault();
-    
-    // get dream value and add it to the list
-    dreams.push(dreamInput.value);
-    appendNewDream(dreamInput.value);
-  
-    // reset form 
-    dreamInput.value = '';
-    dreamInput.focus();
-  };
-  
+  })
 })()
